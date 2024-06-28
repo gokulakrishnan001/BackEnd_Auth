44a5b9d1b965fc73880fdac0ccbc504e0a7c6d7e
gokulakrishnan001 <gokulakrishnan.g@outlook.com>
2024-06-28 18:36:41 +0530

    login completion

--- controller/userController.js
+++ controller/userController.js
@@ -1,40 +1,7 @@
 const model=require('../model/UserDataSchema')
-const crypto = require('crypto');
-const algorithm = 'aes-256-cbc'; //Using AES encryption
-const key = crypto.randomBytes(32);
-const iv = crypto.randomBytes(16);
 const bcrypt=require('bcryptjs')
 
 
-function encrypt(text) {
-    let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
-    let encrypted = cipher.update(text);
-    encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
- }
-
- function decrypt(text) {
-    let iv = Buffer.from(text.iv, 'hex');
-    let encryptedText = Buffer.from(text.encryptedData, 'hex');
-    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
-    let decrypted = decipher.update(encryptedText);
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
-    return decrypted.toString();
- }
-
- function encryption(password){
-    bcrypt.genSalt(10, function (err, Salt) {
-
-        // The bcrypt is used for encrypting password.
-        bcrypt.hash(password, Salt, function (err, hash) {
-    
-            if (err) {
-                return console.log(`Cannot encrypt ${err}`);
-            }
-    
-            return hash
- })})}
- 
  
 
 const createUser=async(req,res)=>{
@@ -65,15 +32,33 @@ const comparePassword=async(req,res)=>{
     try{
         const {username}=req.params
 
+        let status=false
 
-
+      const {password} =req.body
+           
         const [userPassword]=await model.find({username:username})
        
+        bcrypt.compare(password, userPassword.password, (err, isMatch) => { 
+        if( err ) { 
+            return err; 
+        } 
+          
+        // If password matches then display true 
 
+      console.log(isMatch)
+
+        if(isMatch){
+           res.status(200).send({vaildatity:true})
+        }else{
+            res.status(200).send({vaildatity:false})
+        }
+    }); 
 
         console.log(userPassword)
-
-        res.send(userPassword.password)
+       
+        
+         
+        
 
     }catch(err){
 
